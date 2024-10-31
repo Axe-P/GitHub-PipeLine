3eeca53af93c3d86d234452ac41d407a4da00466
Axe-P <axepax72@gmail.com>
2024-10-31 11:11:44 -0600

    test

--- server/src/config/connection.ts
+++ server/src/config/connection.ts
@@ -3,6 +3,11 @@ dotenv.config();
 
 import mongoose from 'mongoose';
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/techquiz');
+const mongoUri = process.env.MONGODB_URI;
+if (!mongoUri) {
+  throw new Error('MONGODB_URI is not defined in the environment variables');
+}
+
+mongoose.connect(mongoUri);
 
 export default mongoose.connection;
